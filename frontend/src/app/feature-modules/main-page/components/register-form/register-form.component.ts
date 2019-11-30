8e1327caf81bfd9b8fccb980dc1b4a99281d92ee
Marek <36851977+browek@users.noreply.github.com>
2019-11-30 19:17:50 +0100

    update register panel

--- frontend/src/app/feature-modules/main-page/components/register-form/register-form.component.ts
+++ frontend/src/app/feature-modules/main-page/components/register-form/register-form.component.ts
@@ -13,6 +13,7 @@ export class RegisterFormComponent implements OnInit {
   registerForm: FormGroup;
   hidingPassword = true;
   hidingRePassword = true;
+  isPasswordWrong = false;
 
   constructor(
     public dialog: MatDialog,
@@ -24,19 +25,23 @@ export class RegisterFormComponent implements OnInit {
   ngOnInit() {
     this.registerForm = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, Validators.required),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
       rePassword: new FormControl(null, Validators.required),
       firstName: new FormControl(null, Validators.required),
       surName: new FormControl(null, Validators.required),
       college: new FormControl(null, Validators.required),
       department: new FormControl(null, Validators.required),
       direction: new FormControl(null, Validators.required),
-      role: new FormControl(false, Validators.required)
+      role: new FormControl('STUDENT', Validators.required)
     });
   }
 
   onSubmit() {
     console.log(this.registerForm);
+    if (this.registerForm.controls.password.value
+                          ===
+      this.registerForm.controls.rePassword.value) {
+    this.isPasswordWrong = false;
     const {
       firstName,
       surName,
@@ -59,8 +64,10 @@ export class RegisterFormComponent implements OnInit {
         college.value
       )
       .subscribe();
+  } else {
+    this.isPasswordWrong = true;
   }
-
+}
   closeDialog(): void {
     this.dialogRef.close();
   }
@@ -68,5 +75,8 @@ export class RegisterFormComponent implements OnInit {
   openLoginDialog(): void {
     this.closeDialog();
     this.dialogService.openLoginDialog();
+  }
+  wrongPassword() {
+
   }
 }
